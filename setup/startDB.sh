099e0d2bd7b0a28d95c151d29f1a7e3a375e746c
lyndonpett <lyndonpett98@gmail.com>
2022-09-09 11:56:12 -0500

    Added pip installs

--- setup/startDB.sh
+++ setup/startDB.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 service mysql start
 
+pip install django
+pip install django-cors-headers
+pip install django-filter
+pip install djangorestframework
+
 echo "CREATE DATABASE IF NOT EXISTS bob_ross;" | mysql -uroot -proot
 
 python3 load.py
