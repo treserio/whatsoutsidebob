f381e39f8c52b28426c6c19b074f357f21321786
Tres Serio <treserio@hotmail.com>
2022-09-13 16:09:21 -0500

    setup login-redirect, changed template names

---
+++ wob/scripts/login-redirect.js
@@ -0,0 +1,35 @@
+// need to check for special chars in auth-token!!! & fix regex if needed
+// document.cookie = "Authorization=Token FAILED"
+// dfcf19a03c33d4e3a5661e61e944c783cc1ad3d4
+authCheck();
+function authCheck() {
+  // grab our authorization string from the cookie to add to the header
+  let Authorization = document.cookie.match(/Authorization=(Token [\w\d]+)/);
+  if (!Authorization) {
+    // need to make sure the document is ready
+    setTimeout(() => {
+      document.querySelector("body").style.visibility = "visible";
+      document.querySelector("#loader").remove();
+    }, 300);
+    return;
+  }
+  Authorization = Authorization[1];
+  // console.log('auth', {Authorization});
+  fetch('http://localhost:8000/api/all_info/', {
+    headers: {Authorization}
+  })
+    .then((res) => {
+      // console.log('res', res);
+      // console.log('status:', res.status);
+      if (res.status == 200) {
+        window.location.replace('http://127.0.0.1:5500/wob/templates/app.html');
+      } else {
+        // remove the cookie and load fix the visibility
+        document.cookie = "Authorization=Token Failed; expires=Sat, 01 Jan 2022 00:00:01 GMT";
+        document.querySelector("body").style.visibility = "visible";
+        document.querySelector("#loader").remove();
+      }
+    });
+}
+
+
